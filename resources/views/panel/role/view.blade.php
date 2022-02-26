b147c549e113928b71577d7a6bcb50a72faab551
zaman shovon <moniruzzaman@guardianlife.com.bd>
2022-02-27 00:43:49 +0600

    Admin role view set and relation has created

--- resources/views/panel/role/view.blade.php
+++ resources/views/panel/role/view.blade.php
@@ -21,7 +21,6 @@
     <div class="card">
       <div class="card-header">
         <h3 class="card-title">Projects</h3>
-
         <div class="card-tools">
           <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
             <i class="fas fa-minus"></i></button>
